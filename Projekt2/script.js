aee304b6c966bb670f8d343c0c07f5b882b20122
Ismail <icmoil2002@gmail.com>
2022-04-05 12:20:32 +0300

    Поправка гита

---
+++ Projekt2/script.js
@@ -0,0 +1,21 @@
+let title = ("Screen layout");
+const screens = ("Простые, Сложные, Интерактивные");
+let screenPrice = 25;
+let rollback = 100;
+let fullPrice = 50;
+const adaptive = false;
+
+console.log(title, fullPrice, adaptive);
+console.log(screens);
+console.log(screenPrice);
+console.log(fullPrice);
+
+let str = "screens"
+
+console.log(str.length);
+
+console.log(str.toLowerCase);
+
+console.log(str.split(" "));
+
+console.log(50 * (100 / 100));
