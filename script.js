1a3e5a8d43f43b5d4baefa9cb2375106b445a4dd
Ismail <icmoil2002@gmail.com>
2022-04-12 19:05:12 +0300

    Final version

---
+++ script.js
@@ -0,0 +1,41 @@
+let title = prompt('Как называется проект', 'Название');
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+screens = screens.toLowerCase().split(', ');
+console.log(screens);
+
+let screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
+let rollback = 25;
+const adaptive = confirm('Нужен ли адаптив для сайта?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'модалка');
+let servicePrice1 = prompt('Сколько это будет стоить?', '5000');
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка2');
+let servicePrice2 = prompt('Сколько это будет стоить?', '6000');
+
+let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+
+let servicePercentPrice = Math.floor(fullPrice - (fullPrice / rollback));
+console.log(servicePercentPrice);
+if (fullPrice > 30000) {
+    console.log('Даем скидку 10%');
+} else if (fullPrice > 15000 && fullPrice < 30000) {
+    console.log('skidka 5%');
+} else if (fullPrice > 0 && fullPrice < 15000) {
+    console.log('Skidka 0');
+} else {
+    console.log('chtoto poshlo ne tak');
+}
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens);
+
+console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
+
+console.log(screens.toLocaleLowerCase().split());
+
+console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)))
+
+
