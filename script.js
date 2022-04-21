7c215bb018291051dd886105a43a3f065025db91
Ismail <icmoil2002@gmail.com>
2022-04-21 09:55:00 +0300

    Lesson05

--- script.js
+++ script.js
@@ -1,66 +1,71 @@
-let title = prompt('Как называется проект', 'Название');
+let title = prompt('Как называется проект', 'Экраны ');
 let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
-
 let screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
 let rollback = 25;
 const adaptive = confirm('Нужен ли адаптив для сайта?');
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'модалка');
-let servicePrice1 = prompt('Сколько это будет стоить?', '5000');
+//let service1 = prompt('Какой дополнительный тип услуги нужен?', 'модалка');
+//let servicePrice1 = prompt('Сколько это будет стоить?', '5000');
+//let service2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка2');
+//let servicePrice2 = prompt('Сколько это будет стоить?', '6000');
 
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка2');
-let servicePrice2 = prompt('Сколько это будет стоить?', '6000');
 
-let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
-
-let servicePercentPrice = Math.floor(fullPrice - (fullPrice / 100 * rollback));
-console.log(servicePercentPrice);
 
 let allServicePrices
 
-
-
 const getAllServicePrices = function () {
-    return service1 + service2
+    let sum = 0
+
+    for (let i = 0; i < 2; i++) {
+        sum += +prompt("Сколько это будет стоить?")
+    }
+
+    return sum
 }
 const showTypeOf = function (variable) {
     console.log(variable, typeof variable)
-}
+};
 const getFullprice = function () {
     return screenPrice + allServicePrices
-}
+};
 const getServicePercentPrice = function () {
     return fullPrice - (fullPrice * (rollback / 100))
-}
+};
 const getTitle = function () {
     return title.trim()[0].toUpperCase() + title.trim().substr(1).toLowerCase()
+};
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+};
+
+const getRollbackMessage = function (price) {
+    if (fullPrice > 30000) {
+        console.log('Даем скидку 10%');
+    } else if (fullPrice > 15000 && fullPrice < 30000) {
+        console.log('Даём скидку 5%');
+    } else if (fullPrice > 0 && fullPrice < 15000) {
+        console.log('Скидка не предусмотрена');
+    } else {
+        console.log('Что-то пошло не так');
+    }
 }
 
+do {
+    screenPrice = +prompt('Сколько будет стоить данная работа?')
+} while (!isNumber(screenPrice));
 
 
-if (fullPrice > 30000) {
-    console.log('Даем скидку 10%');
-} else if (fullPrice > 15000 && fullPrice < 30000) {
-    console.log('Даём скидку 5%');
-} else if (fullPrice > 0 && fullPrice < 15000) {
-    console.log('Скидка не предусмотрена');
-} else {
-    console.log('Что-то пошло не так');
-}
+allServicePrices = getAllServicePrices();
+fullPrice = getFullprice();
+servicePercentPrice = getServicePercentPrice();
+title = getTitle();
 
 
+showTypeOf(title);
+showTypeOf(screenPrice);
+showTypeOf(adaptive);
 
-allServicePrices = getAllServicePrices()
-fullPrice = getFullprice()
-servicePercentPrice = getServicePercentPrice()
-title = getTitle()
-
-
-showTypeOf(title)
-showTypeOf(screenPrice)
-showTypeOf(adaptive)
-
-
+console.log("allServicePrices", allServicePrices);
 
 console.log(typeof title);
 console.log(typeof fullPrice);
