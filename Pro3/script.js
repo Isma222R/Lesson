df04ed380c11ec27e3c9dbe95015aa3c79d74f6e
Ismail <icmoil2002@gmail.com>
2022-04-22 11:22:30 +0300

    Game Lesson

--- Pro3/script.js
+++ Pro3/script.js
@@ -1,34 +1,33 @@
 let title = prompt('Как называется проект', 'Название');
-let screens = prompt('Какие типы экранов нужны', 'Простые, Сложные, Интерактивные');
-let screenPrice = prompt('Сколько будет стоить данная работа?', (12000));
-let rollback = 10000;
-let fullPrice = 23000;
-let servicePercentPrice = 13000;
-const adaptive = false;
+let screens = prompt('Какие типы экранов нужно разработать?', 'Простые, Сложные, Интерактивные');
+screens = screens.toLowerCase().split(', ');
+console.log(screens);
 
-let service1 = prompt('Какой дополнительный тип услуги нужен?', 'модалка')
+let screenPrice = prompt('Сколько будет стоить данная работа?', 12000);
+let rollback = 25;
+const adaptive = confirm('Нужен ли адаптив для сайта?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?', 'модалка');
 let servicePrice1 = prompt('Сколько это будет стоить?', '5000');
 
-let service2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка2')
+let service2 = prompt('Какой дополнительный тип услуги нужен?', 'модалка2');
 let servicePrice2 = prompt('Сколько это будет стоить?', '6000');
 
+let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
 
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-
-console.log('Стоимость верстки экранов ' + screenPrice + ' рублей/ долларов/гривен/юани');
-
-console.log(screens.toLocaleLowerCase().split(', '));
-
-console.log('Процент отката посреднику за работу ' + (fullPrice * (rollback / 100)));
-
-console.log('servicePercentPrice' + '13000');
-
-
-if ('Если fullPrice' > '30000') {
-    console.log('Даем скидку в 10%')
+let servicePercentPrice = Math.floor(fullPrice - (fullPrice / rollback));
+console.log(servicePercentPrice);
+if(fullPrice > 30000){
+    console.log('Даем скидку 10%');
+} else if(fullPrice>15000 && fullPrice<30000){
+    console.log('skidka 5%');
+} else if(fullPrice>0 && fullPrice<15000){
+    console.log('Skidka 0');
+}_else {
+    console.log('chtoto poshlo ne tak');
 }
+
+
 
 
 
